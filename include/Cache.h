b238063ec15ca62ecf7e00ec2b3950042d005031
chenyifei <378802033@qq.com>
2023-05-16 02:03:35 +0000

    cache 多重包含

--- include/Cache.h
+++ include/Cache.h
@@ -1,6 +1,9 @@
+#ifndef CACHE_H
+#define CACHE_H
 #include <iostream>
 #include"MetadataManager.h"
 class Cache{
     public:
         virtual std::string getChunkData(ENTRY_VALUE ev)=0;
 };
+#endif
