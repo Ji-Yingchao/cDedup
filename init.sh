362bb36c7e1d0c0a8f76d05a816f493c9b12eb1a
chenyifei <378802033@qq.com>
2023-08-14 02:13:09 +0000

    init脚本调整

--- init.sh
+++ init.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 # 这里的配置需要和参数json一致
 
-# CDC
-rm -fr TEST/metadata/FileRecipes
-rm -fr TEST/metadata/Containers
-rm -f TEST/metadata/fingerprints.meta
+rm -fr TEST/metadata
 
-# FULL FILE
-rm -fr TEST/metadata/FULL_FILE_STORAGE
-rm -f TEST/metadata/FFFP.meta
-
-# MERKLE TREE
-rm -f TEST/metadata/L1.meta
-rm -f TEST/metadata/L2.meta
-rm -f TEST/metadata/L3.meta
-rm -f TEST/metadata/L4.meta
-rm -f TEST/metadata/L5.meta
-rm -f TEST/metadata/L6.meta
-
-
-
+mkdir TEST/metadata
 mkdir TEST/metadata/FileRecipes
 mkdir TEST/metadata/Containers
-touch TEST/metadata/fingerprints.meta
 mkdir TEST/metadata/FULL_FILE_STORAGE
+
+touch TEST/metadata/fingerprints.meta
 touch TEST/metadata/FFFP.meta
 touch TEST/metadata/L1.meta
 touch TEST/metadata/L2.meta
