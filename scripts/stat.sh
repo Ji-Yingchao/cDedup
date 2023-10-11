70c7837c783eec18731430be96933cdd4cb7a73c
chenyifei <378802033@qq.com>
2023-10-11 14:12:53 +0000

    压缩后大小统计方法更新；

--- scripts/stat.sh
+++ scripts/stat.sh
@@ -1,5 +1,5 @@
-origin_path=/home/cyf/dataHDD/ubuntuImage
-gzip_container_path=/home/cyf/TEST/metadata/ContainersGZIP
+origin_path=/home/cyf/dataHDD/linuxVersion
+gzip_container_path=/home/cyf/cDedup/TEST/metadata/ContainersGZIP
 
 origin_size=$(du -sb $origin_path | awk '{print $1}')
 echo "Origin files size before deduplication: $origin_size bytes"
@@ -7,7 +7,7 @@ echo "Origin files size before deduplication: $origin_size bytes"
 after_deduplication_size=$(ls -l ../TEST/metadata/Containers | grep -v '.r$' | awk '{total+=$5} END {print total}')
 echo "Total container size after deduplication: $after_deduplication_size bytes"
 
-after_compression_size=$(ls -l ../TEST/metadata/ContainersGZIP | awk '{total+=$5} END {print total}')
+after_compression_size=$(du -sb $gzip_container_path | awk '{print $1}')
 echo "Total container size after deduplication and compression: $after_compression_size bytes"
 
 DR=$(echo "scale=2; a=($origin_size-$after_deduplication_size)/$origin_size; if (a < 1) print 0; a" | bc)
