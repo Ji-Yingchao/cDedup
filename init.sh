85ae7fa9e187955c0ec838d06080d27903a4f15a
chenyifei <378802033@qq.com>
2023-12-06 11:29:01 +0000

    元数据目录修正

--- init.sh
+++ init.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # 这里的配置需要和参数json一致
 
-rm -fr TEST/metadata
+rm -fr ./metadata
 
-mkdir TEST/metadata
-mkdir TEST/metadata/FileRecipes
-mkdir TEST/metadata/Containers
-mkdir TEST/metadata/FULL_FILE_STORAGE
+mkdir ./metadata
+mkdir ./metadata/FileRecipes
+mkdir ./metadata/Containers
+mkdir ./metadata/FULL_FILE_STORAGE
 
-touch TEST/metadata/fingerprints.meta
-touch TEST/metadata/FFFP.meta
-touch TEST/metadata/L1.meta
-touch TEST/metadata/L2.meta
-touch TEST/metadata/L3.meta
-touch TEST/metadata/L4.meta
-touch TEST/metadata/L5.meta
-touch TEST/metadata/L6.meta
+touch ./metadata/fingerprints.meta
+touch ./metadata/FFFP.meta
+touch ./metadata/L1.meta
+touch ./metadata/L2.meta
+touch ./metadata/L3.meta
+touch ./metadata/L4.meta
+touch ./metadata/L5.meta
+touch ./metadata/L6.meta
 
 source ./scripts/cleaar_global_stat.sh
 
